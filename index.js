c91a9c9e2c729596845325462c0c12acfec2d657
Chester <chesterdeveloper2000@gmail.com>
2020-07-10 15:20:36 +0800

    completed the app's fuctionality - reading out the items in the directory

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const fs = require("fs")
+
+fs.readdir(".", (err, filenames) => {
+    // either 
+    // err === an error object, which means something went wrong
+    // or 
+    // err === null, which means everything is ok
+    if(err){
+        // error handling code here
+        console.log(err)
+    }
+
+    console.log(filenames)
+})
