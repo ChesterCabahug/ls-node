7e1ab71a2e740b8d8faef28a2ec657f9a1d021b5
Chester <chesterdeveloper2000@gmail.com>
2020-07-11 09:44:20 +0800

    used process.cwd() instead of '.'

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const fs = require("fs")
 
-fs.readdir(".", (err, filenames) => {
-    // either 
-    // err === an error object, which means something went wrong
-    // or 
-    // err === null, which means everything is ok
+fs.readdir(process.cwd(), (err, filenames) => {
     if(err){
-        // error handling code here
         console.log(err)
     }
 
