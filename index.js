9c31cc5d04da25621bdfbc4e6ece3493f5ae5f14
Chester <chesterdeveloper2000@gmail.com>
2020-07-11 11:58:00 +0800

    trying to do solution #2, but first, I've learned about 3 ways on how to use promises

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 #!/usr/bin/env node
 const fs = require("fs")
+const util = require("util")
+
+// // METHOD #2
+// const lstat = util.promisify(fs.lstat)
+
+// METHOD #3
+const {lstat} = fs.promises
 
 fs.readdir(process.cwd(), (err, filenames) => {
     if(err){
         console.log(err)
     }
-    const allStats = Array(filenames.length).fill(null)
-    for (filename of filenames) {
-        const index = filenames.indexOf(filename)
-        fs.lstat(filename, (err, stats) => {
-            if(err) {
-                console.log(err)
-            }
 
-            allStats[index] = stats
+     
 
-            const ready = allStats.every((stats) => {
-                return stats
-            })
-            if(ready) {
-                allStats.forEach((stats, index) => {
-                    console.log(filenames[index], stats.isFile())
-                })
-            }
-        })
-    }
+    // METHOD #1:
+    // const lstat = (filename) => {
+    //     return new Promise((resolve, reject) => {
+    //         fs.lstat(filename, (err, stats) => {
+    //             if(err) {
+    //                 reject(err)
+    //             }
+
+    //             resolve(stats)
+    //         })
+    //     })
+    // }
 })
